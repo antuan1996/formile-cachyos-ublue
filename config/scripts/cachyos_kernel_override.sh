150da3515d5e3e65b579b2bfa1667be86df19dc7
Formile <antonar47@gmail.com>
2024-03-16 10:08:04 +0300

    custom changes

---
+++ config/scripts/cachyos_kernel_override.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'CachyOS kernel override'
+rpm-ostree cliwrap install-to-root / && \
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos    
+# rpm-ostree install kernel-cachyos-lts-devel
+
+# rpm-ostree install libcap-ng-devel procps-ng-devel uksmd-lts
+# systemctl enable uksmd.service
