cee3b721b386c8a14b738f77e07df0ae50ede2f1
Formile <antonar47@gmail.com>
2024-03-17 10:53:23 +0300

    enable ukmsd

--- config/scripts/cachyos_kernel_override.sh
+++ config/scripts/cachyos_kernel_override.sh
@@ -6,10 +6,13 @@
 set -oue pipefail
 
 # Your code goes here.
+echo 'Enable SElinux policy'
+setsebool -P domain_kernel_load_modules on
+
 echo 'CachyOS kernel override'
 rpm-ostree cliwrap install-to-root / && \
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos    
-# rpm-ostree install kernel-cachyos-lts-devel
 
-# rpm-ostree install libcap-ng-devel procps-ng-devel uksmd-lts
-# systemctl enable uksmd.service
+echo 'Install uksmd'
+rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
+systemctl enable uksmd.service
